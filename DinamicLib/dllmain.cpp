a1571c8334059225b2d8ef492361fe1211a50849
KonstantinovArtur <144149895+KonstantinovArtur@users.noreply.github.com>
2024-06-05 13:15:28 +0300

    Add files via upload

---
+++ DinamicLib/dllmain.cpp
@@ -0,0 +1,15 @@
+﻿
+#include "pch.h"
+#include <string>
+using namespace std;
+
+extern "C" __declspec(dllexport) bool AllCharacters(const char* str, const char* chars) {
+    string s(str);
+    string c(chars);
+    for (char ch : c) {
+        if (s.find(ch) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ bibl/bibl.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string S, C;
+    cout << "Ваше слово: ";
+    getline(cin, S);
+    cout << "Символ для поиска: ";
+    getline(cin, C);
+    HINSTANCE load = LoadLibrary(L"DinamicLib.dll");
+  
+    typedef bool (*ContainsAllCharactersFunc)(const char*, const char*);
+    ContainsAllCharactersFunc A = (ContainsAllCharactersFunc)GetProcAddress(load, "AllCharacters");
+   
+    bool result = A(S.c_str(), C.c_str());
+    if (result) {
+        cout << "ЕСТЬ" << endl;
+    }
+    else {
+        cout << "НЕТУ" << endl;
+    }
+    FreeLibrary(load);
+}
